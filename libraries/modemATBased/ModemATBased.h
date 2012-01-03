dd1856079e83a40631fba99d76d57ec3c1a212e0
helmutkemper <helmut.kemper@gmail.com>
2012-01-03 15:13:02 -0200

    Falta testar

--- libraries/modemATBased/ModemATBased.h
+++ libraries/modemATBased/ModemATBased.h
@@ -1,198 +1,143 @@
-#ifndef ModemATBased_h
-#define ModemATBased_h
-
-#if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
-
-#define debug_ModemATBased
-//#define I_do_not_need_to_send_sms_in_my_program
-//#define I_do_not_need_to_internet_in_my_program
-
-//#define kTelefon    0x80
-//#define kMessage    0x81
-//#define kAnyWare    0x82
-
-/*! \brief All Arduino Serial Ports.
- *  Sets the serial port used to communicate with the module.
- *  The UBRRxH are AVR registers used by serial port.
- *  
- *  Define a porta de serial usada para se comunicar com o módulo.
- *  UBRRxH são registros do AVR usados para configurar a porta serial
- *  
- */
-namespace SerialPort
-{
-	enum eSerialPort
-	{
-		#if defined(UBRR3H)
-		Port3,
-		#endif
-		#if defined(UBRR2H)
-		Port2,
-		#endif
-		#if defined(UBRR1H)
-		Port1,
-		#endif
-		#ifndef debug_sayitmodule
-		Port0
-		#else
-		#warning SayItModule::SerialPort0 is used by debuger. Please, open SayItModule.h and comment line #define debug_sayitmodule 1
-		#warning SayItModule::SerialPort0 esta sendo usada pelo debuger. Por favor, abra o arquivo SayItModule.h e comente a linha #define debug_sayitmodule 1
-		#endif
-	};
-}
-using namespace SerialPort;
-
-namespace Event
-{
-    enum eEvent
-    {
-        None,
-        Ring,
-        SMSNew,
-        SMSSend,
-        SMSRead,
-        InternetConnect,
-        InternetConnectToHost,
-        InternetDataSendByGET,
-        InternetDisconnectToHost,
-        NoCarrier,
-        NoDialTone,
-        NoAnswer,
-        Busy,
-        CmeError,
-        Error,
-        Close,
-        Closed,
-        ConnectionFailed,
-        CallReady,
-        PowerDown,
-        IdCaptured,
-        DayCaptured,
-        MonthCaptured,
-        YearCaptured,
-        HourCaptured,
-        MinuteCaptured,
-        SecondCaptured,
-        TelefonCaptured,
-        MessageCaptured,
-        StatusCaptured,
-        TimeZoneCaptured,
-        
-        internetConnectFunction,
-        internetDisconnectToHostFunction,
-        internetConnectToHostFunction,
-        internetDataSendByGETFunction,
-        sendTextSmsFunction
-    };
-}
-using namespace Event;
-
-extern "C"
-{
-	typedef void ( * ponteiroDeFuncao )( eEvent, eEvent );
-}
-
-
-
-const byte modem_read_expected_response         		=   0;
-const byte modem_read_no_carrier                		=   1;
-const byte modem_read_no_dialtone               		=   2;
-const byte modem_read_no_dial_tone              		=   3;
-const byte modem_read_answer                    		=   4;
-const byte modem_read_busy                      		=   5;
-const byte modem_read_cme_error                 		=   6;
-const byte modem_read_error                     		=   7;
-const byte modem_read_ring                      		=   8;
-const byte modem_read_new_sms                   		=   9;
-const byte modem_read_close                     		=  10;
-const byte modem_read_closed                    		=  11;
-const byte modem_read_connection_failed         		=  12;
-const byte modem_read_call_ready                		=  13;
-const byte modem_read_normal_power_down         		=  14;
-const byte modem_read_continue                  		=  15;
-const byte modem_read_sms_read                  		=  16;
-const byte modem_read_capturing_anything				=  17;
-const byte modem_read_capturing_anything_started		=  18;
-const byte modem_read_capturing_anything_ended			=  19;
-const byte modem_read_capture_ignore_anything_number    =  20;
-
 /*
- *  Please, note: change this values, afect "ModemATBased::clearFlags ()"
- */
-const byte modem_read_clear_data                =  26;
-const byte modem_read_capturing_number_started  =  27;
 
-const byte modem_read_capturing_number_ended    =  28;//sem uso
-const byte modem_read_capturing_quoted          =  29;
-const byte modem_read_capturing_quoted_started  =  30;
-const byte modem_read_capturing_quoted_ended    =  31;
+AT+CMGF=1
+AT+CMGR=20
 
-class ModemATBased
-{
-	private:
-	
-        static void ( * vcpfOnFunction ) ( void );
++CMGR: "REC READ","08197344690","","12/01/01,20:10:43-12"
+Hello World! In Brasil, this message can be 128 characters per message
+
+OK
+
+
+
+
+como ler a qualidade de sinal
+at+csq
+resposta:
++CSQ: n,m
+se n = 0 sinal = -115dBm ou mais
+     = 1       = -111dBm
+     = 2...30  = -(2*n+54)dBm
+     = 31      = -52dBm
+     = 99      = desconectado ou muito baixo
+     
+COMO LER O NOME DA OEPRADORA
+AT+CSPN?
+RESPOSTA
++CSPN: "OPERADORA", N°
+
+COMO LER O NUMERO DE QUEM ESTA LIGANDO
+AT+CLCC
+RESPOSTA:
++CLCC: 1,1,4,0,0,"99268744",161,""
+*/
+
+
+// Please note, in AT String, '&' is used to capture number to memory ( equivales to exp reg '[\-\+0-9]+' )
+//                            '@o' is used 'turn ignore all spaces' on ( equivales to exp reg '[\t\s]' ) - caution: \r or \n turn 'ignore all spaces' off
+//                            '@f' is used 'turn ignore all spaces' off
+//                            '%' is used to capture quoted String
+//     						  '$' is used to capture any data, except new line ( equivales to exp reg '^.*?[\r\n]' )
+//                            'i' is used to indicate Id address
+//                            'd' is used to indicate Day address
+//                            'M' is used to indicate Month address
+//                            'y' is used to indicate Year address
+//                            'h' is used to indicate Hour address
+//                            'm' is used to indicate Minute address
+//                            's' is used to indicate Second address
+//                            't' is used to indicate Telefon address
+//                            'g' is used to indicate Message address
+//                            'S' is used to indicate Status address
+//                            'z' is used to indicate Time Zone address
+// Example: "+CMTI: \"SM\",&i", where, '&i' is 'capture number and put in Id address'
+
+#ifndef ModemATList_h
+#define ModemATList_h
+
+const String modem_ip_single					 =  "AT+CIPMUX=0\r";//OK
+//const String modem_ip_multi						 =  "AT+CIPMUX=1\r";//OK
+const String modem_ip_mode_normal				 =  "AT+CIPMODE=0\r";//OK-ERROR
+//const String modem_ip_mode_transparent			 =  "AT+CIPMODE=1\r";//OK-ERROR
+const String modem_internet_connect				 =  "AT+CGDCONT=1,\"IP\",\"TATA.DOCOMO.INTERNET\",\"10.6.6.6\",0,0\r";//OK-ERROR
+const String modem_interner_connect_password	 =  "AT+CSTT=\"TATA.DOCOMO.INTERNET\"\r";//OK_ERROR
+const String modem_deactive_gprs_dpd			 =  "AT+CIPSHUT\r";//OK-CLOSE OK-ERROR
+const String modem_deactive_gprs_dpd_ok			 =  "SHUT OK\r";//OK-CLOSE OK-ERROR
+
+const String modem_connect_host_and_port_1of3	 =  "AT+CIPSTART=\"TCP\",\"";//OK-+CME ERRORn-ALREADY CONNECT-STATE:n-CONNECT FAIL-CONNECT OK
+const String modem_connect_host_and_port_2of3	 =  "\",\"";
+const String modem_connect_host_and_port_3of3	 =  "\"\r";
+const String modem_connect_host_and_port_ok      =  "CONNECT OK\r";
+
+const String modem_start_send_data_over_tcp_udp	 =  "AT+CIPSEND\r";
+
+const String modem_header_get_send_1of3			 =  "GET ";
+const String modem_header_get_send_2of3          =  "q";
+const String modem_header_get_send_3of3			 =  " HTTP/1.1\r\n";
+const String modem_header_host					 =  "Host: ";
+const String modem_header_hostString             =  "H";
+const String modem_header_hostPort				 =  "p";
+const String modem_heafer_connection_alive		 =  "Connection: Keep-Alive\r\n";
+const String modem_header_accept				 =  "Accept: */*\r\n";
+const String modem_header_connection_close		 =  "Connection: close\r\n\r\n";
+const String modem_header_connection_close_ok	 =  "SEND OK\r";
+
+const String modem_at_command_general_end_line   =  "\r\n";
+
+
+//const String modem_sms_read_all					 =  "CMGL=\"ALL\"";
+//const String modem_sms_delete_by_id				 =  "CMGD=";
+//const String modem_sms_read_by_id				 =  "CMGR=";
+//const String modem_sms_new_message				 =  "CNMI:";
+//const String modem_modem_status					 =  "CPAS";//0 call ready - 2 unknow - 3 tocando - 4 em ligacao
+//const String modem_modem_ring					 =  "RING";
+//const String modem_modem_no_carrier				 =  "NO CARRIER";
+const String modem_modem_ok						 =  "OK\r";
+const String modem_modem_error					 =  "ERROR\r";
+
+const String modem_sms_text_redy_to_send		 =  "> ";
+
+// Comandos de 'echo' on/off faz o modem repetir tudo o que é enviado. 
+// Deve ser a primeira coisa envoada ao modem
+const String modem_echo_off						 =  "ATE0\r";//OK
+//const String modem_echo_on						 =  "ATE1\r";//OK
+
+    #ifndef I_do_not_need_to_send_sms_in_my_program
     
-		static eSerialPort		vceSerial;
-        static eEvent           vceEvent;
-        static eEvent           vceEventDispatchedBy;
-		static const String *	vcacucATString[ 11 ];
-		static const String *	vcacucATResponse[ 11 ];
+        const String modem_sms_text_mode				 =  "AT+CMGF=1\r";//OK
+        const String modem_sms_send_confg_1of2           =  "AT+CMGS=\"";
+        const String modem_sms_send_telefon              =  "t";
+        const String modem_sms_send_confg_2of2           =  "\"\r";
+        const String modem_sms_send_message              =  "g";
+        const String modem_sms_read_message              =  "AT+CMGR=";
+        const String modem_response_sms_read             =  "+CMGR: %S,\"&t\",\"\",\"&y/&M/&d,&h:&m:&s&z\"\r";
         
-		static unsigned char	vcucSMStep;
-        static unsigned char    vcucSMStepCompare;
-        static unsigned char    vcucSMTotalStep;
-        static          char    vcascPointerDataModem;
+    #endif
     
-		static void				sendData ( String vapucData );
-		static int				availableData ( );
-		static unsigned char	getData ( );
-		static void				StateMachineRun ();
-        static void             clearFlags ();
-        
-        static void             testSpecialCharacter ( unsigned char * vapucSerialData, String * vapstVariable, eEvent vaenEvent );
-        static void             testCharacterAndRunStateMachine ( unsigned char * vapucSerialData, const String * vapcstsATCommand, const byte * vapcstbtFlagAddress );
-        static void             testCharacterAndMakeEvent ( unsigned char * vapucSerialData, const String * vapcstsATCommand, const byte * vapcstbtFlagAddress, eEvent vaenEvent );
-		
-	public:
-        
-        static unsigned long    vculFlags;
-        
-        static ponteiroDeFuncao StateMachineEvent;
-        static String           Host;
-        static String           HostPort;
-        static String           QueryString;
-        
-                                ModemATBased ();
-		static void				setSerial ( eSerialPort vaeSerial, unsigned int vauiSpeed );
-        
-        #ifndef I_do_not_need_to_send_sms_in_my_program
-        
-            static String       Id;
-            static String       Day;
-            static String       Month;
-            static String       Year;
-            static String       Hour;
-            static String       Minute;
-            static String       Second;
-            static String       Telefon;
-            static String       Message;
-            static String       Status;
-            static String       TimeZone;
-            static void			sendTextSms ();
-            static void         readTextSms ();
-        
-        #endif
-        
-        static void             internetDataSendByGET ();
-        static void             internetDisconnectToHost ();
-        static void             internetConnectToHost ();
-        static void             internetConnect ();
-		static void				getDataModem ();
-};
+const String modem_variable_id                       =  "i";
+    
+const String modem_bye                               =  String(char(0x1A));
+const String modem_bye_ok                            =  "SEND OK\r\n";
+//const String modem_sms_send_ok                     =  "";
 
+const String modem_response_no_carrier               =  "NO CARRIER\r";
+const String modem_response_no_dialtone1             =  "NO DIALTONE\r";
+const String modem_response_no_dialtone2             =  "NO DIAL TONE\r";
+const String modem_response_no_answer                =  "NO ANSWER\r";
+const String modem_response_busy                     =  "BUSY\r";
+const String modem_response_cme_error                =  "+CME ERROR\r";
+const String modem_response_error                    =  "ERROR\r";
+const String modem_response_ring                     =  "RING\r";
+const String modem_response_sms                      =  "+CMTI: \"SM\",&i\r";
+const String modem_response_close                    =  "CLOSE\r";
+const String modem_response_closed                   =  "CLOSED\r";
+
+const String modem_response_connection_failed        =  "CONNECTION FAILED\r";
+const String modem_response_call_ready               =  "Call Ready\r";
+const String modem_response_power_down               =  "NORMAL POWER DOWN\r";
+
+const String modem_user_response_data				 =  "&data:";
+const String modem_user_response_telefon			 =  "@o&telefon:";
+const String modem_user_response_id					 =  "@o&id:";
+const String modem_user_response_message			 =  "&message:";
+const String modem_user_response_status				 =  "&status:";
 #endif
